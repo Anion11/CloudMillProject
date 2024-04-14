a90167c35a14aa45be4d2cd92ddb7e45a38e5266
Nikita Klyukoit <klukoyt@dterra.ru>
2024-04-14 22:59:51 +0300

    small fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,15 @@ import '../styles/style.scss';
 import 'virtual:svg-icons-register';
 import 'lazysizes';
 import { select } from '../blocks/select/select';
-import { input } from '../blocks/input/input'
-import { header } from '../blocks/header/header'
+import { input } from '../blocks/input/input';
+import { header } from '../blocks/header/header';
 import { footer } from '../blocks/footer/footer';
+import { contactCard } from '../blocks/cards/contact-card/contact-card';
 
 document.addEventListener('DOMContentLoaded', function () {
   select();
   input();
   header();
   footer();
+  contactCard();
 });
